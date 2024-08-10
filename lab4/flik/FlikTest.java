1859c751f5d43ea5ac35e2cb24925a9f53634c77
vv <1975290977@qq.com>
2024-08-11 02:13:38 +0800

    done lab4

---
+++ lab4/flik/FlikTest.java
@@ -0,0 +1,10 @@
+package flik;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class FlikTest {
+    @Test
+    public void test() {
+        assertTrue(Flik.isSameNumber(129,129));
+    }
+}
--- lab4/flik/HorribleSteve.java
+++ lab4/flik/HorribleSteve.java
@@ -5,6 +5,7 @@ public class HorribleSteve {
         int i = 0;
         for (int j = 0; i < 500; ++i, ++j) {
             if (!Flik.isSameNumber(i, j)) {
+                System.out.println(Flik.isSameNumber(i, j));
                 throw new Exception(
                         String.format("i:%d not same as j:%d ??", i, j));
             }
